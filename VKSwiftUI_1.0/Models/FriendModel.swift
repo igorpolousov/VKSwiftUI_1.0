83df34ed7b596046a6dc8fff757c6307471dfc04
Igor Polousov <igorpolousov@gmail.com>
2022-05-11 13:34:52 +0300

    + friend struct + friend demo array

--- VKSwiftUI_1.0/Models/FriendModel.swift
+++ VKSwiftUI_1.0/Models/FriendModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Combine
+
+// Массив данных полученных из JSON
+var friends = [Friend]()
+
+// MARK: - Welcome
+struct FriendsContainer: Codable {
+    let response: FriendsResponse
+}
+
+// MARK: - Response
+struct FriendsResponse: Codable {
+    let items: [Friend]
+}
+
+// MARK: - Friend
+struct Friend: Codable, Identifiable {
+    let id: Double
+    let lastName: String
+    let photo50: String
+    let firstName: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case lastName = "last_name"
+        case photo50 = "photo_200_orig"
+        case firstName = "first_name"
+    }
+}
+
+let friendsDemoData: [Friend] = [
+    Friend(id: 1, lastName: "Крокодил", photo50: "gena", firstName: "Гена"),
+    Friend(id: 2, lastName: "Знатный", photo50: "chebu", firstName: "Чебурашка"),
+    Friend(id: 3, lastName: "Пух", photo50: "vinni", firstName: "Винни")
+]
--- VKSwiftUI_1.0/Views/FriendsView.swift
+++ VKSwiftUI_1.0/Views/FriendsView.swift
@@ -5,4 +5,22 @@
 //  Created by Igor Polousov on 11.05.2022.
 //
 
-import Foundation
+import SwiftUI
+
+struct FriendsView: View {
+    
+    var body: some View {
+        List(friends.sorted(by: {$0.firstName < $1.firstName})) { friend in
+            Text("\(friend.firstName) \(friend.lastName)")
+        }
+        .navigationTitle("Friends")
+    }
+}
+
+
+
+struct FriendsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendsView()
+    }
+}
