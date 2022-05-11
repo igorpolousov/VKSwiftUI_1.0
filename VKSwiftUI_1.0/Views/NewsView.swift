cdb15d79d5d9e3df506c400dca047c17a438a2cf
Igor Polousov <igorpolousov@gmail.com>
2022-05-11 17:34:52 +0300

    + news view

--- VKSwiftUI_1.0/Views/NewsView.swift
+++ VKSwiftUI_1.0/Views/NewsView.swift
@@ -10,48 +10,59 @@ import Combine
 
 struct NewsView: View {
     var body: some View {
-        VStack {
-            AvatarNameDate()
-            NewsDescriptionAndImage()
+        
+        List(news.sorted(by: {$0.newsDate < $1.newsDate})) { newsItem in
+            VStack {
+                AvatarNameDate(avatarImage: newsItem.userImage, userName: newsItem.userName, newsDate: newsItem.newsDate)
+                    .padding(.top, 8)
+                NewsDescriptionAndImage(newsImage: newsItem.newsImage, newsText: newsItem.newsText)
+            }
+            .border(.gray, width: 0.8)
+            .cornerRadius(2)
         }
-        .border(.gray, width: 0.8)
-        .cornerRadius(2)
-}
+        
+    }
 
 
 
 
     struct AvatarNameDate: View {
         
+        var avatarImage: String
+        var userName: String
+        var newsDate:String
         
         var body: some View {
             HStack {
-                Image("wti")
+                Image(avatarImage)
                     .resizable()
                     .frame(width: 40, height: 40, alignment: .center)
                     .modifier(CircleShadow(shadowColor: .gray, shadowRadius: 4))
                 VStack {
-                    Text("User name")
-                    Text("News Date")
+                    Text(userName).font(.system(size: 16))
+                    Text(newsDate).font(.system(size: 16))
                 }
             }
-            .frame(width: 320, height: 50, alignment: .topLeading)
-            .padding(.leading, 5)
+            .frame(width: 290, height: 30, alignment: .topLeading)
         }
     }
     
     struct NewsDescriptionAndImage: View {
+        
+        var newsImage: String
+        var newsText: String
+        
         var body: some View {
             VStack {
-                Image("dollars")
+                Image(newsImage)
                     .resizable()
-                    .frame(width: 320, height: 200, alignment: .topLeading)
+                    .frame(width: 290, height: 200, alignment: .topLeading)
                     .padding()
-                Text("Here you'll see news text")
-                    .frame(width: 320, height: 200, alignment: .topLeading)
+                Text(newsText).font(.system(size: 12))
+                    .frame(width: 290, height: 170, alignment: .topLeading)
                     
             }
-            .padding(.leading, 20)
+            .padding(.leading, 0)
         }
     }
 }
