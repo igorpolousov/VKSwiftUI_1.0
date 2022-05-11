fdee9b3721fab1147bc6d8c5d505775a83d18107
Igor Polousov <igorpolousov@gmail.com>
2022-05-11 17:49:38 +0300

    + web view

--- VKSwiftUI_1.0/Views/WebView.swift
+++ VKSwiftUI_1.0/Views/WebView.swift
@@ -5,4 +5,18 @@
 //  Created by Igor Polousov on 11.05.2022.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    var request: URLRequest
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(request)
+    }
+}
