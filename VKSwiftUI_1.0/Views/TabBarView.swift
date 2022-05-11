4f51269e0cdb55f09fc3fad503269cf74ddc5974
Igor Polousov <igorpolousov@gmail.com>
2022-05-11 17:01:50 +0300

    + tab bar view

--- VKSwiftUI_1.0/Views/TabBarView.swift
+++ VKSwiftUI_1.0/Views/TabBarView.swift
@@ -5,4 +5,33 @@
 //  Created by Igor Polousov on 11.05.2022.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+struct TabBarView: View {
+    var body: some View {
+        TabView {
+            FriendsView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Friends")
+                }
+            GroupsView()
+                .tabItem {
+                    Image(systemName: "person.3.fill")
+                    Text("Groups")
+                }
+            NewsView()
+                .tabItem {
+                    Image(systemName: "tray.2")
+                    Text("News")
+                }
+        }
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
