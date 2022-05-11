4be5d94ee0c7a4044f6b5f08d3106e1619a27326
Igor Polousov <igorpolousov@gmail.com>
2022-05-11 12:02:47 +0300

    + files + folders

---
+++ VKSwiftUI_1.0/Models/GroupModel.swift
@@ -0,0 +1,8 @@
+//
+//  GroupModel.swift
+//  VKSwiftUI_1.0
+//
+//  Created by Igor Polousov on 11.05.2022.
+//
+
+import Foundation
---
+++ VKSwiftUI_1.0/Views/FriendDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  FriendDetailView.swift
+//  VKSwiftUI_1.0
+//
+//  Created by Igor Polousov on 11.05.2022.
+//
+
+import Foundation
