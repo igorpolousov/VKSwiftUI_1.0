61adc2f7c48115de1045f180976245f6c7d546e5
Igor Polousov <igorpolousov@gmail.com>
2022-05-11 14:15:41 +0300

    + friend detail view struct

--- VKSwiftUI_1.0/Views/FriendDetailView.swift
+++ VKSwiftUI_1.0/Views/FriendDetailView.swift
@@ -5,4 +5,22 @@
 //  Created by Igor Polousov on 11.05.2022.
 //
 
-import Foundation
+import SwiftUI
+
+struct FriendDetailView: View {
+    var body: some View {
+        Image("gena")
+            .resizable()
+            .frame(width: 300, height: 400, alignment: .center)
+            .cornerRadius(30)
+            .shadow(color: .gray, radius: 10, x: 10, y: 10)
+
+    }
+}
+
+
+struct FriendDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendDetailView()
+    }
+}
